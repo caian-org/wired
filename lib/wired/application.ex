ee4310976ec235e3bf22c90c7eb3d519d9bdae26
Caian R. Ertl <hi@caian.org>
2021-03-23 04:38:52 -0300

    chore: re-init as a supervised Elixir application

---
+++ lib/wired/application.ex
@@ -0,0 +1,20 @@
+defmodule Wired.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Wired.Worker.start_link(arg)
+      # {Wired.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Wired.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
